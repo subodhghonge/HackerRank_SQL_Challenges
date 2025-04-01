764f2feb3e699bd935c396f592e1c1371e2b4619
subodhghonge <subodhghonge12@gmail.com>
2025-04-01 16:15:38 +0530

    Query

---
+++ Basic Select/Select All.sql
@@ -0,0 +1,3 @@
+-- Question - Query all columns (attributes) for every row in the CITY table.
+
+SELECT * FROM CITY;
---
+++ Basic Select/Select By ID.sql
@@ -0,0 +1,4 @@
+-- Question - Query all columns for a city in CITY with the ID 1661.
+
+SELECT * FROM CITY
+WHERE ID = 1661;
